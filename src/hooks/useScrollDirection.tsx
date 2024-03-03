5eab5896aef52c569f6d67ca1718448df7eb53dd
Kristine Tomlinson <ktomlinson@labx.com>
2024-03-03 16:16:02 -0500

    feat: scrolling header

---
+++ src/hooks/useScrollDirection.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+const useScrollDirection = () => {
+  const threshold = 100; // how far user scrolls before we trigger the header shrink
+  const [scrollDir, setScrollDir] = useState("up");
+
+  useEffect(() => {
+    let previousScrollYPosition = window.scrollY;
+
+    const scrolledMoreThanThreshold = (currentScrollYPosition: number) =>
+      Math.abs(currentScrollYPosition - previousScrollYPosition) > threshold;
+
+    const isScrollingUp = (currentScrollYPosition: number) =>
+      currentScrollYPosition > previousScrollYPosition &&
+      !(previousScrollYPosition > 0 && currentScrollYPosition === 0) &&
+      !(currentScrollYPosition > 0 && previousScrollYPosition === 0);
+
+    const updateScrollDirection = () => {
+      const currentScrollYPosition = window.scrollY;
+
+      if (scrolledMoreThanThreshold(currentScrollYPosition)) {
+        const newScrollDirection = isScrollingUp(currentScrollYPosition) ? "down" : "up";
+        setScrollDir(newScrollDirection);
+        previousScrollYPosition = currentScrollYPosition > 0 ? currentScrollYPosition : 0;
+      }
+    };
+
+    const onScroll = () => window.requestAnimationFrame(updateScrollDirection);
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  return scrollDir;
+};
+
+export default useScrollDirection;
